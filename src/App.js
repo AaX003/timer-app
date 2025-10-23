5407e8fa207859dd5cee7b33ed7be63b1749a079
AX-oais <aax.hill2003@outlook.com>
2025-10-22 21:28:23 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState(Math.floor(Math.random() * 1000000));
+  const [paused, setPaused] = useState(false);
+
+  useEffect(() => {
+    if (paused) return;
+
+    const interval = setInterval(() => {
+      setTime((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(interval);
+          return 0;
+        }
+        return prevTime - 1;
+      })
+    }, 1000)
+      return () => clearInterval(interval);
+  }, [paused]);
+
+  const prev = useRef(time);
+
+  const pauseTime = () => {
+    setPaused(true);
+  }
+
+  const resumeTime = () => {
+    setPaused(false);
+  }
+
+  const resetTime = () => {
+    setTime(prev.current);
+    setPaused(false);
+  }
+
+  const days = Math.floor(time / 86400); 
+  const hours = Math.floor((time % 86400) / 3600);
+  const mins = Math.floor((time % 3600) / 60);
+  const secs = time % 60;
+
+  const format = (t) => String(t).padStart(2, "0");
+
+  // const timeDisplay = `${days.toString().padStart(2, "0")}:${hours.toString().padStart(2, "0")}:${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <section className="time-container">
+        <header className="timer-text">
+          <small className="text">
+            Days | Hours | Mins | Secs
+          </small>
+        </header>
+        <h1 className="time">
+         <span className="days">{format(days)}</span>:<span className="hours">{format(hours)}</span>:<span className="mins">{format(mins)}</span>:<span className="secs">{format(secs)}</span>
+        </h1>
+      </section>
+      <section className="action-btns-container">
+        <button className={paused ? "resume" : "paused"} onClick={paused ? resumeTime : pauseTime} disabled={time === 0}>
+          {paused ? "Resume" : "Pause"}
+        </button>
+        <button className="reset" onClick={resetTime}>
+          Reset
+        </button>
+      </section>
     </div>
   );
 }
